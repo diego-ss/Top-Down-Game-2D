5eae78239ed8b6f3c3ef6fb7a575fc33640b3a3e
Diego <diego_sousa.s@outlook.com>
2023-11-11 17:51:39 -0300

    feat: criando personagem e movimentando ele

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 200.0f;
+    
+    private Rigidbody2D rigidbody;
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = direction.normalized * speed * Time.fixedDeltaTime;
+    }
+}
