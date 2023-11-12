ec64712693282300f4b7771b5263b595b6e364d0
Diego <diego_sousa.s@outlook.com>
2023-11-12 16:25:44 -0300

    feat: implementação inicial de um sistema de dialogos

---
+++ Assets/Scripts/Dialog/DialogueSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue/Dialogue", order = 1)]
+public class DialogueSettings : ScriptableObject
+{
+    [Header("Settings")]
+    public GameObject actor;
+
+    [Header("Dialogue")]
+    public Sprite speakerSprite;
+    public string sentence;
+
+    public List<Sentence> dialogues = new List<Sentence>();
+}
+
+[Serializable]
+public class Sentence
+{
+    public string actorName;
+    public Sprite profile;
+    public Languages sentence;
+}
+
+[Serializable]
+public class Languages
+{
+    public string portuguese;
+    public string english;
+    public string spanish;
+}
