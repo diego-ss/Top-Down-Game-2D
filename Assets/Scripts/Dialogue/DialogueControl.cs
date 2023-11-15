b1264fcd88b7b1d47a6582f71044e1cb690d5d7e
Diego <diego_sousa.s@outlook.com>
2023-11-15 00:02:15 -0300

    feat: continuação da implementação do sistema de diálogos

---
+++ Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [Header("Components")]
+    public GameObject dialogueContainer; // janeça do dialogo
+    public Image profileSprite; // sprite do perfil do personagem
+    public Text dialogueText; // texto do dialogo
+    public Text actorNameText; // nome do personagem
+
+    [Header("Settings")]
+    public float typingSpeed = 0.05f; // velocidade de digitação do texto
+
+    // Variáveis de controle
+    private bool isShowing; // se o dialogo está sendo mostrado
+    private int index; // indice do texto atual
+    private string[] sentences; // lista de textos
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TypeSentence()
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    /// <summary>
+    /// pular para próxima fala
+    /// </summary>
+    public void NextSentence()
+    {
+
+    }
+
+    /// <summary>
+    /// chamar dialogo do personagem
+    /// </summary>
+    public void Speech(string[] txt)
+    {
+        if(!isShowing)
+        {
+            dialogueContainer.SetActive(true);
+            sentences = txt;
+            isShowing = true;
+            StartCoroutine(TypeSentence());
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Dialogue : MonoBehaviour
+{
+    public float dialogueDistance = 2f;
+    public LayerMask playerLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLayer = LayerMask.GetMask("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ShowDialogue();
+    }
+
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueDistance, playerLayer);
+
+        if(hit != null)
+        {
+            Debug.Log("Player can talk to NPC");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueDistance);
+    }
+}
