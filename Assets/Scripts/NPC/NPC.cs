361e47223ab1ad8c38928c6f05a28bbdd31eef9a
Diego <diego_sousa.s@outlook.com>
2023-11-18 19:30:56 -0300

    feat: primeira implementação da IA de andar dos npcs

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public float speed;
+    public List<Transform> targets = new List<Transform>();
+    
+    private int index = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targets[index].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targets[index].position) < 0.1f)
+        {
+            if (index < targets.Count - 1)
+                index++;
+            else
+                index = 0;
+        }
+
+        Vector2 direction = targets[index].position - transform.position;
+        if (direction.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        } else if (direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+}
