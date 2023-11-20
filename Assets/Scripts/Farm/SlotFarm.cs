d72a5ecf9bc8aa30813dc1c6a7bf7cd97c2dd87b
Diego <diego_sousa.s@outlook.com>
2023-11-20 17:12:08 -0300

    feat: cavando e criando buraco

---
+++ Assets/Scripts/Farm/SlotFarm.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotFarm : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite hole;
+    [SerializeField] private Sprite carrot;
+
+    [SerializeField] private int digAmount = 3; // quantidade de vezes que o jogador deve cavar para abrir o buraco
+    private int initialDigAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialDigAmount = digAmount;    
+        spriteRenderer = transform.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public void OnHit()
+    {
+        digAmount--;
+
+        if (digAmount <= 0)
+        {
+            spriteRenderer.sprite = hole;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Shovel"))
+            OnHit();
+    }
+}
