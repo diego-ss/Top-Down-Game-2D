4841d7a49049dc85fd224bd2040b00433078e7a0
Diego <diego_sousa.s@outlook.com>
2023-11-25 13:03:48 -0300

    feat: colisão do ataque do inimigo ao player

--- Assets/Scripts/Enemy/Skeleton/AnimationControl.cs
+++ Assets/Scripts/Enemy/Skeleton/AnimationControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AnimationControl : MonoBehaviour
 {
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask playerLayer;
     private Animator animator;
 
     // Start is called before the first frame update
@@ -21,5 +24,24 @@ public class AnimationControl : MonoBehaviour
     public void PlayAnimation(int value)
     {
         animator.SetInteger("transition", value);
+    }
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+        
+        if(hit != null )
+        {
+            // tira vida do player
+            Debug.Log("Acertou o player");
+        } else
+        {
+            // não acertou o player
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
