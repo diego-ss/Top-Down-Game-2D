0eea2889d3246f4e878c9271952a2159c01df6ab
Diego <diego_sousa.s@outlook.com>
2023-11-25 12:44:25 -0300

    feat: ativando animações e rotacionando o inimigo

--- Assets/Scripts/Enemy/Skeleton/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton/Skeleton.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Skeleton : MonoBehaviour
 {
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private AnimationControl animationControl;
+
+    private Player player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<Player>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.transform.position);
+
+        if(transform.position.x > player.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Vector2.Distance(transform.position, player.gameObject.transform.position) <= agent.stoppingDistance)
+        {
+            // chegou no player
+            animationControl.PlayAnimation(2);
+        }
+        else
+        {
+            // animação de walk
+            animationControl.PlayAnimation(1);
+        }
     }
 }
