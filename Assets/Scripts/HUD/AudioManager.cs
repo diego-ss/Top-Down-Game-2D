84a57b1c73d3738103a64eb34fb5c71d8445e1b6
Diego <diego_sousa.s@outlook.com>
2023-11-27 09:36:20 -0300

    feat: sfx para background e ações do player

---
+++ Assets/Scripts/HUD/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField] private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject); // Don't destroy this object when loading a new scene
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }   
+
+    public void SwitchAndPlay(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
